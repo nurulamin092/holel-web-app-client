e0912e8d1ab634d39ee1a3b8e712f1129d6c52fe
nurulamin092 <mnurulamin092@gmial.com>
2022-10-15 12:14:52 +0600

    subscribe component added

--- src/Pages/Subscribe.js
+++ src/Pages/Subscribe.js
@@ -8,7 +8,7 @@ const Subscribe = () => {
                 <h2 className='text-[33px] font-bold text-center'>Subscribe to Our Newsletter <br /> <span className='text-sm'>Don’t miss this opportunity. Subscribe now</span></h2>
 
                 <div className='flex mt-10 justify-center'>
-                    <input type="text" placeholder="Enter your email address" className="input input-bordered" />
+                    <input type="text"placeholder="Enter your email address" className="input input-bordered" />
                     <button className=' bg-primary'>Subscribe</button>
                    
                 </div>
